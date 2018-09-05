2ae5d5aec725b03b3c4338d454258b41d4921cad
susana <susana.zv@gmail.com>
2018-09-05 19:32:32 -0300

    2nd commit

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -9,21 +9,24 @@ class SalesController < ApplicationController
 
   def create
     @sale = Sale.new(sale_params)
-    total = @sale.value - (@sale.value * (@sale.discount / 100.0)
+    total = @sale.value - (@sale.value * (@sale.discount / 100.0))
     @sale.value = total.to_i
-      if @sale.tax == 1
-       total = @sale.value - (@sale.value * 0.19)
-       @sale.tax = 19
-       end
+    if @sale.tax == 1
+      total = @sale.value - (@sale.value * 0.19)
+      @sale.tax = 19
+    end
     @sale.save
     redirect_to sales_path
   end
+
+  
 
   def done
     @sale = Sale.all
   end
 
   private
-  def sale_params
+ def sale_params
     params.require(:sale).permit(:cod,:detail,:category,:value,:discount,:tax,:total)
   end
+end
